f89296d4a1adbb382506da4402f084921d65bea9
Unknown <cdromboxset@outlook.com>
2017-11-04 21:59:13 -0500

    getRolesAllServers
    Added function to list Roles
    Added some ToDo items

--- BotTest.py
+++ BotTest.py
@@ -1,11 +1,19 @@
 #Tutorial https://www.youtube.com/watch?v=u6tBvQSXJ7I
+#Add to server link: https://discordapp.com/oauth2/authorize?client_id=your_client_id_goes_here&scope=bot&permissions=0
 #Python wrapper https://pypi.python.org/pypi/discord.py/
 #Note that in Python 3.4 you use `@asyncio.coroutine` instead of `async def` and `yield from` instead of `await`.
 #Python 3.5+ use `async def`
 #Another tutorial that MIGHT be useful https://www.youtube.com/watch?v=aFI1SItR8tg
 
 #TODO:
-#-Look into pass_context, figure out if that's a way to capture reference to last message
+#-Make getChannels organize by server. Probs use a dictionary here.
+#-Make the bot post only to a specific channel.
+#--If the context is outside of the specified channel, tag the author.
+#-MAYBE refactor all these "gets" to helpers, make the current ones "say" functions
+#-Make getRoles build a list and return it in sorted alphabetical
+
+#Dictionary
+# ServerName : ([Roles], [Channels])
 
 import discord
 from discord.ext.commands import Bot
@@ -54,6 +62,19 @@ async def getChannels():
     for c in channelList:
     	channelString += str(c.name) + " in server, " + str(c.server.name) + "\n"
     await bot.say(channelString)
+
+@bot.command()
+async def getRolesAllServers():
+	await bot.say("I can see the following roles: ")
+	responseString = ""
+	serverList = list(bot.servers)
+	for s in serverList:
+		responseString += str(s.name) + "\n"
+		for role in s.roles:
+			if role.is_everyone is not True:
+				responseString += "- " + str(role.name) + "\n"
+		responseString += "\n"
+	await bot.say(responseString)
 
 @bot.command()
 async def getServersRaw():
